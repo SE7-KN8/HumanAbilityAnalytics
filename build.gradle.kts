6968d453cea3851e52b6722a013b46d31909435b
Sebastian Knackstedt <sebknack@gmail.com>
2020-09-23 21:34:34 +0200

    Create fat jar.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("application")
     id("org.openjfx.javafxplugin") version "0.0.8"
 }
+
 group = "com.github.se7_kn8"
 version = "1.0-SNAPSHOT"
 
@@ -12,15 +13,28 @@ repositories {
     mavenCentral()
 }
 
-tasks.withType<KotlinCompile>() {
+// TODO include javafx runtime dependencies to make this cross platform
+dependencies {
+}
+
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
 javafx {
     version = "15"
-    modules = listOf("javafx.base", "javafx.controls", "javafx.graphics", "javafx.web")
+    modules = listOf("javafx.base", "javafx.graphics")
 }
 
 application {
     mainClassName = "App"
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "AppKt"
+    }
+    from(
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    )
 }
